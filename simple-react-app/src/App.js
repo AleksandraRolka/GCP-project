6f2e13f8aeb01252e5ee6ca9d1a07d641a07fb70
arolka <arolka@interia.com>
2023-01-11 00:58:28 +0100

    Init simple app with docker

--- simple-react-app/src/App.js
+++ simple-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TODO: simple app to upload image with few information to cloud
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
